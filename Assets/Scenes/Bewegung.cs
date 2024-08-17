56c60329b9dd348c7f74d1630fc2745c512d0305
Milla <jugj@users.noreply.github.com>
2024-08-17 12:33:52 +0200

    Änderungen von Milla um 12:33:52

---
+++ Assets/Scenes/Bewegung.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bewegung : MonoBehaviour
+{
+    public float Geschwindigkeit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         
+    if(Input.GetKey("left")){
+       transform.Translate(Vector2.left*Geschwindigkeit*Time.deltaTime,Space.World);
+    }
+    if(Input.GetKey("right")){
+       transform.Translate(Vector2.right*Geschwindigkeit*Time.deltaTime,Space.World);
+   }
+    }
+}
