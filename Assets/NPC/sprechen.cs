f46361141ac94b222441ebf34d070caa05960719
Marina <jugj@users.noreply.github.com>
2024-08-17 15:09:25 +0200

    Änderungen von Marina um 15:09:25

--- Assets/NPC/sprechen.cs
+++ Assets/NPC/sprechen.cs
@@ -5,7 +5,13 @@ using UnityEngine;
 public class sprechen : MonoBehaviour
 {
     public GameObject Spieler;
+<<<<<<< Updated upstream:Assets/NPC/sprechen.cs
 
+=======
+    // Start is called before the first frame update
+    
+    // Update is called once per frame
+>>>>>>> Stashed changes:Assets/sprechen.cs
     void Update()
     {
         float distance = Vector2.Distance (Spieler.transform.position, gameObject.transform.position);
