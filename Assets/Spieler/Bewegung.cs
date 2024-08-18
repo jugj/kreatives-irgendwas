9992684a2855e5b9bc78fab3e67977c06db14a1c
Ceci <jugj@users.noreply.github.com>
2024-08-18 13:32:47 +0200

    Änderungen von Ceci um 13:32:47

--- Assets/Spieler/Bewegung.cs
+++ Assets/Spieler/Bewegung.cs
@@ -5,31 +5,46 @@ using UnityEngine;
 public class Bewegung : MonoBehaviour
 {
     public float Geschwindigkeit;
-    private bool walk;
+    private Animator anim;
 
+  void Start (){
+    anim = GetComponent<Animator>();
+  }
   void Update(){
-      if  (Input.GetKey("up")){
+    if  (Input.GetKey("up")){
         transform.Translate(Vector2.up*Geschwindigkeit*Time.deltaTime,Space.World);
-        walk = true;
     }
-     if(Input.GetKey("down")){
+    if(Input.GetKey("down")){
        transform.Translate(Vector2.down*Geschwindigkeit*Time.deltaTime,Space.World);
-       walk = true;
     }
     if(Input.GetKey("left")){
        transform.Translate(Vector2.left*Geschwindigkeit*Time.deltaTime,Space.World);    
-       transform.localScale = new Vector2(-1f, 1f);
-       walk = true;
-         
+       transform.localScale = new Vector2(-1f, 1f);        
     }
     if(Input.GetKey("right")){
        transform.Translate(Vector2.right*Geschwindigkeit*Time.deltaTime,Space.World);
        transform.localScale = new Vector2(1f, 1f);
-       walk = true;
     }
 
+    bool walk;
+
+    if (Input.GetKey("up")){
+      walk = true;
+    }
+    else if (Input.GetKey("down")){
+      walk = true;
+    }
+    else if (Input.GetKey("left")){
+      walk = true;
+    }
+    else if (Input.GetKey("right")){
+      walk = true;
+    }
     else {
       walk = false;
     }
+
+anim.SetBool("walk",walk);
+
   } 
 }
