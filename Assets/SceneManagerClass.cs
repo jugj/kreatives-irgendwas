4f8008dfb1bc64c7fcfc7d8dc02d585143e9d9d8
Jonas Tamimi <oas@users.noreply.github.com>
2024-08-18 19:55:38 +0200

    Updated SceneManager

--- Assets/SceneManagerClass.cs
+++ Assets/SceneManagerClass.cs
@@ -6,35 +6,35 @@ using UnityEngine.SceneManagement;
 public class SceneManagerCLass : MonoBehaviour
 {
     public void LoadDialog1(){
-        UnityEngine.SceneManagement.SceneManager.LoadScene(0);
-    }
-
-    public void LoadDialog2(){
         UnityEngine.SceneManagement.SceneManager.LoadScene(1);
     }
 
-    public void LoadDialog3(){
+    public void LoadDialog2(){
         UnityEngine.SceneManagement.SceneManager.LoadScene(2);
     }
 
-    public void LoadDialog4(){
+    public void LoadDialog3(){
         UnityEngine.SceneManagement.SceneManager.LoadScene(3);
     }
 
-    public void LoadStadt1(){
-        UnityEngine.SceneManagement.SceneManager.LoadScene(6);
+    public void LoadDialog4(){
+        UnityEngine.SceneManagement.SceneManager.LoadScene(4);
     }
 
-    public void LoadStadt2(){
+    public void LoadStadt1(){
         UnityEngine.SceneManagement.SceneManager.LoadScene(7);
     }
 
-    public void LoadStadt3(){
+    public void LoadStadt2(){
         UnityEngine.SceneManagement.SceneManager.LoadScene(8);
     }
 
-    public void LoadTitleScreen1(){
+    public void LoadStadt3(){
         UnityEngine.SceneManagement.SceneManager.LoadScene(9);
+    }
+
+    public void LoadTitleScreen1(){
+        UnityEngine.SceneManagement.SceneManager.LoadScene(0);
     }
 
     public void LoadTitleScreen2(){
