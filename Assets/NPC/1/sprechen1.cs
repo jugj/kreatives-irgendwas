75dc23e0088c3b2fb68f6b7bb40d990591c778e5
Marina <jugj@users.noreply.github.com>
2024-08-18 14:48:55 +0200

    Änderungen von Marina um 14:48:54

--- Assets/NPC/1/sprechen1.cs
+++ Assets/NPC/1/sprechen1.cs
@@ -7,13 +7,24 @@ public class sprechen1 : MonoBehaviour
 {
     public SceneManagerCLass SceneManagerCLass;
     public GameObject spieler;
+    public GameObject button;
     public bool bereitsGesprochen=false;
     // Update is called once per frame
     void Update()
     {
+        if((Vector2.Distance(gameObject.transform.position, spieler.transform.position)<1)){
+            button.SetActive(true);
+            Debug.Log("an");
+        }
+        else{
+            button.SetActive(false);
+            Debug.Log("aus");
+        }
+
         if((Vector2.Distance(gameObject.transform.position, spieler.transform.position)<1)&&(Input.GetKey("space"))){
             bereitsGesprochen=true;
             SceneManagerCLass.LoadDialog1();
+            button.SetActive(true);
         }
     }
 }
--- Assets/NPC/2/sprechen2.cs
+++ Assets/NPC/2/sprechen2.cs
@@ -8,9 +8,19 @@ public class sprechen2 : MonoBehaviour
     public SceneManagerCLass SceneManagerCLass;
     public GameObject spieler;
     public bool bereitsGesprochen=false;
+    public GameObject button;
     // Update is called once per frame
     void Update()
     {
+        if((Vector2.Distance(gameObject.transform.position, spieler.transform.position)<1)){
+            button.SetActive(true);
+            Debug.Log("an");
+        }
+        else{
+            button.SetActive(false);
+            Debug.Log("aus");
+        }
+
         if((Vector2.Distance(gameObject.transform.position, spieler.transform.position)<1)&&(Input.GetKey("space"))){
             bereitsGesprochen=true;
             SceneManagerCLass.LoadDialog3();
