327d65aa65ab29c23bb0dbbafd36dc80d50399b6
Ceci <jugj@users.noreply.github.com>
2024-08-18 12:25:24 +0200

    xx

--- Assets/Spieler/Bewegung.cs
+++ Assets/Spieler/Bewegung.cs
@@ -5,18 +5,31 @@ using UnityEngine;
 public class Bewegung : MonoBehaviour
 {
     public float Geschwindigkeit;
+    private bool walk;
+
   void Update(){
       if  (Input.GetKey("up")){
         transform.Translate(Vector2.up*Geschwindigkeit*Time.deltaTime,Space.World);
+        walk = true;
     }
      if(Input.GetKey("down")){
        transform.Translate(Vector2.down*Geschwindigkeit*Time.deltaTime,Space.World);
+       walk = true;
     }
     if(Input.GetKey("left")){
-       transform.Translate(Vector2.left*Geschwindigkeit*Time.deltaTime,Space.World);
+       transform.Translate(Vector2.left*Geschwindigkeit*Time.deltaTime,Space.World);    
+       transform.localScale = new Vector2(-1f, 1f);
+       walk = true;
+         
     }
     if(Input.GetKey("right")){
        transform.Translate(Vector2.right*Geschwindigkeit*Time.deltaTime,Space.World);
+       transform.localScale = new Vector2(1f, 1f);
+       walk = true;
+    }
+
+    else {
+      walk = false;
     }
   } 
 }
